10b8a6782148988a3cbf7b220de75be3ba5059f9
SvenKayser <sk@urbanhosting.de>
2017-03-15 18:08:05 +0100

    1.3.1-12 - Now uses MCF'S EventBus to let Plugins do their jobs

--- src/main/java/com/jeffpeng/jmod/hungeroverhaul/Plugin.java
+++ src/main/java/com/jeffpeng/jmod/hungeroverhaul/Plugin.java
@@ -5,16 +5,26 @@ import iguanaman.hungeroverhaul.config.IguanaConfig;
 
 import java.util.Map;
 
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.Potion;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.EntityInteractEvent;
 
 import com.jeffpeng.jmod.JMODPlugin;
 import com.jeffpeng.jmod.JMODPluginContainer;
 import com.jeffpeng.jmod.crafting.ToolUnbreaker;
+import com.jeffpeng.jmod.forgeevents.JMODAddBuffsEvent;
+import com.jeffpeng.jmod.types.items.CoreBucket;
 
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.eventhandler.EventPriority;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class Plugin extends JMODPlugin {
 
@@ -23,14 +33,40 @@ public class Plugin extends JMODPlugin {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void addBuffs(Map<String, Potion> buffMap){
-		if(Loader.isModLoaded("HungerOverhaul")) buffMap.put("wellfed", HungerOverhaul.potionWellFed);
+	@SubscribeEvent
+	public void addBuffs(JMODAddBuffsEvent e){
+		if(Loader.isModLoaded("HungerOverhaul")) e.buffMap.put("wellfed", HungerOverhaul.potionWellFed);
 	}
 	
-	@Override
-	public void on(FMLInitializationEvent event){
-		if(Loader.isModLoaded("HungerOverhaul")) MinecraftForge.EVENT_BUS.register(new EventHandlers());
+	@SubscribeEvent(priority=EventPriority.HIGH)
+	public void milkCow(EntityInteractEvent event){
+		if(!Loader.isModLoaded("HungerOverhaul")) return;
+		EntityPlayer ep = event.entityPlayer;
+		ItemStack is = event.entityPlayer.getCurrentEquippedItem();
+		if(is == null) return;
+		Item item = is.getItem();
+		if(!(item instanceof CoreBucket) || !((CoreBucket)item).hasBucketsOf("milk")) return;
+		
+		if((event.target instanceof EntityCow) && is.getItemDamage() == 0){
+			NBTTagCompound tags = event.target.getEntityData();
+			if (tags.hasKey("Milked"))
+			{
+				System.out.println(tags.getInteger("Milked"));
+				event.setCanceled(true);
+				if (!ep.worldObj.isRemote)
+				event.target.playSound("mob.cow.hurt",  0.4F, 0.9F);
+				return;
+			} else tags.setInteger("Milked", IguanaConfig.milkedTimeout * 60);
+				
+			ep.getCurrentEquippedItem().stackSize--;
+			if (!ep.inventory.addItemStackToInventory(((CoreBucket)item).getBucketOf("milk")))
+            {
+                ep.dropPlayerItemWithRandomChoice(((CoreBucket)item).getBucketOf("milk"), false);
+            }
+		}
 	}
+	
+
 	
 	@Override
 	public void on(FMLPostInitializationEvent event) {
